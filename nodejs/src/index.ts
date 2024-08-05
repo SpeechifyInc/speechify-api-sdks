41aff762108c5993ac4d5fb43bebceda73ecb7af
Eugene Mirotin <emirotin@gmail.com>
2024-08-05 16:09:54 +0300

    scaffold the class

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -1,21 +1,38 @@
-export interface Example {
-	x: number;
+export interface SpeechifyOptions {
+	strict?: boolean;
+	apiKey?: string;
 }
 
-export function exampleFunction(): Example {
-	return { x: 1 };
-}
+export class Speechify {
+	#strict: boolean;
+	#apiKey: string | undefined;
 
-export class ExampleClass {
-	constructor() {
-		console.log("ExampleClass constructor");
+	constructor(options: SpeechifyOptions) {
+		this.#strict = options.strict ?? true;
+
+		this.#checkEnvironment(!!options.apiKey);
+		this.#apiKey = options.apiKey;
 	}
 
-	#examplePrivateMethod() {
-		console.log("ExampleClass private method");
-	}
+	#checkEnvironment(isApiKeySet: boolean) {
+		const isBrowser = typeof window !== "undefined";
 
-	exampleMethod() {
-		console.log("ExampleClass method");
+		/* @todo handle more known clients like Electron apps if we can */
+		const isPublicClient = isBrowser;
+
+		if (!isPublicClient || !isApiKeySet) {
+			return;
+		}
+
+		const message = `
+			You are using the API key in the browser environment.
+			This is strictly not recommended, as it is exposing your Speechify account for anyone to use.
+			Instead, use the API key in a server environment or use the Access Token in the browser.
+			Read more about this at https://docs.sws.speechify.com/docs/authentication`;
+
+		if (this.#strict) {
+			throw new Error(message);
+		}
+		console.warn(message);
 	}
 }
