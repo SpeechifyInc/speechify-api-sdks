5f221f1154b1f329b531a687ffba44ff7544351d
Eugene Mirotin <emirotin@gmail.com>
2024-11-22 15:14:54 +0200

    make sourcemap nicer

--- nodejs/scripts/build.sh
+++ nodejs/scripts/build.sh
@@ -21,14 +21,22 @@ echo Compilation successful!
 
 # After building the package, replace version.ts importing from package.json with the "static" version
 echo Substituting materialized version export
+
 mv lib/cjs/src/version_dist.js lib/cjs/src/version.js
 mv lib/cjs/src/version_dist.js.map lib/cjs/src/version.js.map
 rm lib/cjs/src/version_dist.d.ts
+sed -i '' 's/version_dist.js/version.js/g' lib/cjs/src/version.js.map
+sed -i '' 's/version_dist.js.map/version.js.map/g' lib/cjs/src/version.js
+
 mv lib/esm/src/version_dist.js lib/esm/src/version.js
 mv lib/esm/src/version_dist.js.map lib/esm/src/version.js.map
 rm lib/esm/src/version_dist.d.ts
+sed -i '' 's/version_dist.js/version.js/g' lib/esm/src/version.js.map
+sed -i '' 's/version_dist.js.map/version.js.map/g' lib/esm/src/version.js
 
 echo Cleaning up...
 
-rm -rf lib/{cjs,esm}/**/{*.test.*,test-helper.*,*.tsbuildinfo} > /dev/null 2>&1
+rm -rf lib/{cjs,esm}/**/{*.test.*,test-helper.*} > /dev/null 2>&1
 rm -rf lib/{cjs,esm}/*.tsbuildinfo > /dev/null 2>&1
+
+echo All done
