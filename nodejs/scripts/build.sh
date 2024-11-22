407d705adbd41ec1c6674c2bf18104069c42105b
Eugene Mirotin <emirotin@gmail.com>
2024-11-22 15:04:03 +0200

    fix: don't import JSON at runtime

--- nodejs/scripts/build.sh
+++ nodejs/scripts/build.sh
@@ -3,7 +3,12 @@ set -e
 VERSION=$(node -p "require('./package.json').version")
 echo "Building version $VERSION"
 
-echo Compiling...
+# Some bundlers (Vite+Remix) make it harder to import JSON
+# Because of this, this hack is implemented, substituting the version file
+echo Materializing version file
+echo "export const VERSION = \"$VERSION\";" > src/version_dist.ts
+
+echo Compiling package...
 
 rm -rf lib > /dev/null 2>&1
 tsc -p tsconfig.json --module nodenext --outDir lib/esm
@@ -14,6 +19,16 @@ echo "{\"type\": \"commonjs\", \"version\": \"$VERSION\"}" > lib/cjs/package.jso
 
 echo Compilation successful!
 
+# After building the package, replace version.ts importing from package.json with the "static" version
+echo Substituting materialized version export
+mv lib/cjs/src/version_dist.js lib/cjs/src/version.js
+mv lib/cjs/src/version_dist.js.map lib/cjs/src/version.js.map
+rm lib/cjs/src/version_dist.d.ts
+mv lib/esm/src/version_dist.js lib/esm/src/version.js
+mv lib/esm/src/version_dist.js.map lib/esm/src/version.js.map
+rm lib/esm/src/version_dist.d.ts
+
 echo Cleaning up...
 
 rm -rf lib/{cjs,esm}/**/{*.test.*,test-helper.*,*.tsbuildinfo} > /dev/null 2>&1
+rm -rf lib/{cjs,esm}/*.tsbuildinfo > /dev/null 2>&1
